b7f59c8258199fd748992c6fb46ad74cf9893270
montipallarols <montaga.pallarols@gmail.com>
2020-11-27 12:45:37 +0100

    Create endpoint to get all listItems

--- routers/lists.js
+++ routers/lists.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
 const List = require("../models").list;
+const ListItem = require("../models").listItem;
+const Item = require("../models").item;
+const Profile = require("../models").profile;
 const router = new Router();
 
 
@@ -7,6 +10,15 @@ router.get("/", async (req, res, next) => {
     try {
       const allLists = await List.findAll();
       res.json(allLists);
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get("/listItems", async (req, res, next) => {
+    try {
+      const allListItems = await ListItem.findAll({include: [List, Item]});
+      res.json(allListItems);
     } catch (e) {
       next(e);
     }
