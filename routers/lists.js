80765f334bbf7c91c08b97d93872583ec6c167cc
montipallarols <montaga.pallarols@gmail.com>
2020-12-01 16:40:21 +0100

    Set up endpoint to remove item from Wishlist

--- routers/lists.js
+++ routers/lists.js
@@ -141,5 +141,36 @@ router.get("/", async (req, res, next) => {
     }
   });
 
+  router.delete("/wishlist/listItems/:itemApiId", async (req, res, next) => {
+    try {
+      const itemApiId = req.params.itemApiId
+      const foundListItem = await ListItem.findOne({
+        include: [{
+          model: Item,
+          where: {
+            apiId: itemApiId
+          }
+        }, {
+          model: List,
+          where: {
+            type: "Wishlist"
+          }
+      }]});
+      console.log("FOUND LIST ITEM", foundListItem)
+        const thisListItem = foundListItem.dataValues
+        console.log("THIS list item", thisListItem)
+      if (!foundListItem) {
+        res.status(404).send("List item not found!")
+      } else {
+        await ListItem.destroy({
+          where: { id: thisListItem.id }
+         });
+        res.json(thisListItem);
+      }
+    } catch (e) {
+      next(e);
+    }
+  });
+
 
 module.exports = router;
