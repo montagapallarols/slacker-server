b017cbfc04d9710e460035ebd36bdaccafb7497e
montipallarols <montaga.pallarols@gmail.com>
2020-12-02 11:59:03 +0100

    Set up endpoint to delete item from Favourites list

--- routers/lists.js
+++ routers/lists.js
@@ -173,4 +173,36 @@ router.get("/", async (req, res, next) => {
   });
 
 
+  router.delete("/favourites/listItems/:itemApiId", async (req, res, next) => {
+    try {
+      const itemApiId = req.params.itemApiId
+      const foundListItem = await ListItem.findOne({
+        include: [{
+          model: Item,
+          where: {
+            apiId: itemApiId
+          }
+        }, {
+          model: List,
+          where: {
+            type: "Favourites"
+          }
+      }]});
+      console.log("FOUND LIST ITEM", foundListItem)
+        const thisListItem = foundListItem.dataValues
+        console.log("THIS list item", thisListItem)
+      if (!foundListItem) {
+        res.status(404).send("List item not found!")
+      } else {
+        await ListItem.destroy({
+          where: { id: thisListItem.id }
+         });
+        res.json(thisListItem);
+      }
+    } catch (e) {
+      next(e);
+    }
+  });
+
+
 module.exports = router;
